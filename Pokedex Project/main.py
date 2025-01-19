26bced4c6bed4a1ca491d26ff5b7e5a432a17935
alankuruczBCE <alan.kurucz@bcestudent.com>
2025-01-19 16:56:00 +0000

    Updated Ver.
    version from a while back i was making in college

--- Pokedex Project/main.py
+++ Pokedex Project/main.py
@@ -6,12 +6,15 @@ from window_functions import search_pokemon_
 from PIL import ImageFont
 from tkinter import ttk
 import tkinter as tk
+import customtkinter as ctk
 
 import sv_ttk
 #Read the csv and put into a variable
 pokedex = "pokemon_data.csv"
 data = pd.read_csv(pokedex)
 currentScene = "Main"
+
+
 
 def hide_me(event):
     event.widget.pack_forget()
@@ -38,8 +41,13 @@ def search_():
 
 def filter_pokemon_():
     global currentScene
-    query = entry.get().strip().lower()
+    global frameButtons
+    frameButtons.configure(height=900, width=1200)
+    if currentScene == "PokeDetails":
+        pokeNameLabel.pack_forget()
+        framePokeDetails.pack_forget()
     currentScene = "PokeList"
+    query = entry.get().strip().lower()
     filteredData = data[
         data['Type 1'].str.lower().str.contains(query) |
         data['Type 2'].str.lower().str.contains(query)
@@ -52,26 +60,24 @@ def filter_pokemon_():
 
     for index, row in filteredData.iterrows():
         name = row["Name"]
-        button = ttk.Button(frameButtons, text = name, width = 40, command = lambda name = name: on_button_click(name))
+        button = ctk.CTkButton(frameButtons, fg_color = "#147285", corner_radius=3,text = name, width = 40, command = lambda name = name: on_button_click(name))
         button.pack(pady=5)
 
-
-
-
     frameButtons.grid_propagate(True)
-
+    canvas.pack_forget()
+    canvas.pack( fill = "both", expand = True)
     canvas.config(scrollregion=frameButtons.bbox("all"))
 
 
 def on_button_click(name):
     global currentScene
-    print(f"Clicked on {name}")
     for widget in frameButtons.winfo_children():
         widget.destroy()
     currentScene = "PokeDetails"
     framePokeDetails.pack(side = "top", anchor = "nw", padx = 10, pady = 10)
     pokeNameLabel.pack(padx = 5, pady = 5)
-    pokeNameLabel.config(text = f"Name: {name}")
+    pokeNameLabel.configure(text = "Name: " + name)
+    frameButtons.configure(height = 900, width = 1200)
 
 
 def on_mouse_wheel(event):
@@ -88,9 +94,14 @@ def back_button_():
         print()
     if currentScene == "PokeDetails":
         print("detail")
+        frameButtons.configure(height=900, width=1200)
         pokeNameLabel.pack_forget()
         framePokeDetails.pack_forget()
+        for widget in frameButtons.winfo_children():
+            widget.destroy()
+        currentScene = "Main"
     if currentScene == "PokeList":
+        frameButtons.configure(height=900, width=1200)
         for widget in frameButtons.winfo_children():
             widget.destroy()
         currentScene = "Main"
@@ -113,9 +124,12 @@ def hide_me_(event):
 
 
 
-root = tk.Tk()
+root = ctk.CTk()
 root.title("Pokedex")
-root.geometry("700x700")
+root.geometry("1200x900")
+root.configure(bg="#b34448", fg_color="#b34448")
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
 #set theme here!!!!
 fontPath = "JUST Sans Regular.otf"
 customFont = ImageFont.truetype(fontPath, 10)
@@ -123,20 +137,21 @@ fontName = customFont.getname()[0]
 font1 = font.Font(family = fontName, size = 10)
 
 #Create the title label
-label = ttk.Label(root, text = "Pokedex")
+
+
+searchFrame = ctk.CTkFrame(root, fg_color= "#380b0d", bg_color= "#380b0d", border_width = 0)
+searchFrame.pack(fill = "x")
+label = ctk.CTkLabel(searchFrame, text = "Pokedex")
 label.pack(pady = 10)
-
-
-searchFrame = ttk.Frame(root)
-searchFrame.pack(pady = 10, padx = 10, fill = "x")
 #Create the entry field
-entry = ttk.Entry(searchFrame, width = 40,)
-entry.pack(side = "left", padx = (0,10))
 
-searchButton = ttk.Button(searchFrame, text = "Search",
+entry = ctk.CTkEntry(searchFrame, width = 900,)
+entry.pack(side = "left", padx = 10, pady = 5)
+
+searchButton = ctk.CTkButton(searchFrame, text = "Search",
                              command = filter_pokemon_,
-                          width=150,)
-searchButton.pack(side = "left")
+                          width=300, fg_color="#963035")
+searchButton.pack(side = "left", padx = 10, pady = 5)
 
 #comboboxVar = ttk.StringVar(value="Type")
 #combobox = tk.ComboBox(searchFrame, values=["Type", "Name"],
@@ -148,42 +163,45 @@ searchButton.pack(side = "left")
 
 #######################THIS IS TKINTER DO A CASCADE INSTEAD OF THIS!!!!!!!!!!!!!
 
-backFrame = ttk.Frame(root)
-backFrame.pack(fill = "x")
-backButton = ttk.Button(backFrame, text = "Home", command = back_button_,
-                            width = 200,)
-backButton.pack(side = "bottom", pady=10)
-canvas = tk.Canvas(root, bg = "black")
+canvas = tk.Canvas(root, bg="#b34448", borderwidth=0, highlightthickness=0)
 canvas.pack( fill = "both", expand = True)
 
+backFrame = ctk.CTkFrame(root)
+backFrame.pack(fill = "x")
+backButton = ctk.CTkButton(backFrame, text = "Home", command = back_button_,
+                            width = 200,)
+backButton.pack(side = "bottom", pady=10)
 
 
-frameButtons = ttk.Frame(canvas, width = 700, height = 700,)
 
-scrollbar = ttk.Scrollbar(canvas, orient = "vertical", command = canvas.yview)
+frameButtons = ctk.CTkFrame(canvas, width = 1205, height = 900, fg_color= "#b34448", border_width=0,)
+
+
+scrollbar = ctk.CTkScrollbar(canvas, command = canvas.yview)
 scrollbar.pack(side = "right", fill = "y")
 canvas.configure(yscrollcommand = scrollbar.set)
 
-canvas.create_window((0, 0), window=frameButtons, anchor="nw", width = 700)
+canvas.create_window((0, 0), window=frameButtons, anchor="nw", width = 1200)
 
 frameButtons.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
 root.bind_all("<MouseWheel>", on_mouse_wheel)
 #
 
-framePokeDetails = ttk.Frame(canvas, width = 700, height = 700)
+framePokeDetails = ctk.CTkFrame(canvas, width = 1200, height = 900)
 framePokeDetails.pack(side = "top", anchor = "nw", padx = 10, pady = 10)
 framePokeDetails.pack_forget()
 #Create the search button
-pokeNameLabel = ttk.Label(framePokeDetails, text = "No Name Set")
+pokeNameLabel = ctk.CTkLabel(framePokeDetails, text = "No Name Set")
 pokeNameLabel.pack(padx = 5, pady = 5)
 pokeNameLabel.pack_forget()
 
 
 
-resultLabel = ttk.Label(root, text = "", justify ="left", font = (font1, 10))
-resultLabel.place(x=20, y=100)
-typeLabel = ttk.Label(root, text = "", justify ="left", font = (font1, 10))
-typeLabel.place(x=20, y=130)
+resultLabel = ctk.CTkLabel(root,text="",justify="left",font=(font1, 10))
+resultLabel.place(x=20,y=100)
+resultLabel.pack_forget()
+typeLabel = ttk.Label(root,text="",justify="left",font=(font1, 10))
+typeLabel.place(x=20,y=130)
+typeLabel.pack_forget()
 
-sv_ttk.set_theme("dark")
 root.mainloop()
