85969e85c39b9b0fe40c103e4d3f018f2a631127
alankuruczBCE <alan.kurucz@bcestudent.com>
2025-01-21 11:41:17 +0000

    added goods.
    goods.

--- Pokedex Project/main.py
+++ Pokedex Project/main.py
@@ -13,10 +13,9 @@ import tkinter as tk
 import customtkinter as ctk
 
 #Read the csv and put into a variable
-pokedex = "pokemon_data.csv"
-data = pd.read_csv(pokedex)
-currentScene = "Main"
-
+pokedex="pokemon_data.csv"
+data=pd.read_csv(pokedex)
+currentScene="Main"
 
 
 def hide_me(event):
@@ -25,78 +24,117 @@ def hide_me(event):
 
 def search_():
     scrollbar.pack(side="right", fill="y")
-    query = entry.get()
-
-    result = search_pokemon_(data, query)
+    query=entry.get()
+    result=search_pokemon_(data, query)
 
     if result:
-        pokeNameString = f"Name: {result["Name"]}"
-        pokeTypeString = f"Type: {result["Type 1"]}"
+        pokeNameString=f"Name: {result["Name"]}"
+        pokeTypeString=f"Type: {result["Type 1"]}"
 
     resultLabel.config(text=pokeNameString)
     typeLabel.config(text=pokeTypeString)
 
-
 def filter_pokemon_():
     global currentScene
     global frameButtons
-    frameButtons.configure(height=900, width=1200)
+    frameButtons.configure(height=600, width=600)
     typeDistButton.pack_forget()
-    if currentScene == "PokeDetails":
+    if currentScene=="PokeDetails":
         pokeNameLabel.pack_forget()
         framePokeDetails.pack_forget()
-    currentScene = "PokeList"
-    query = entry.get().strip().lower()
-    filteredData = data[
+    currentScene="PokeList"
+    query=entry.get().strip().lower()
+    filteredData=data[
         data['Type 1'].str.lower().str.contains(query) |
         data['Type 2'].str.lower().str.contains(query)
     ]
 
-    filteredData = filteredData.drop_duplicates(subset=['Name', 'Type 1', 'Type 2'])
-
+    filteredData=filteredData.drop_duplicates(subset=['Name', 'Type 1', 'Type 2'])
     #Destroys all buttons from previous query
     remove_pokemon_()
 
     for index, row in filteredData.iterrows():
-        name = row["Name"]
-        type1 = row["Type 1"]
-        type2 = row.get("Type 2", None)
-        hp = row["HP"]
-        button = ctk.CTkButton(frameButtons, fg_color = "#147285",
-                               corner_radius=3,text = name, width = 40,
-                               command = lambda name = name,
-                                                type1 = type1,
-                               type2 = type2,
-                               hp = hp:
-                               on_button_click(name, type1, type2, hp))
+        name=row["Name"]
+        type1=row["Type 1"]
+        type2=row.get("Type 2", None)
+        hp=row["HP"]
+        atk=row["Attack"]
+        defense=row["Defense"]
+        spAtk=row["Sp. Atk"]
+        spDef=row["Sp. Def"]
+        spd=row["Speed"]
+        gen=row["Generation"]
+        leg=row["Legendary"]
+        button=ctk.CTkButton(frameButtons,fg_color="#147285",
+                               corner_radius=3,text=name,width=40,
+                               command=lambda name=name,
+                                                type1=type1,
+                               type2=type2,
+                               hp=hp,
+                               atk=atk,
+                               defense=defense,
+                               spAtk=spAtk,
+                               spDef=spDef,
+                               spd=spd,
+                               gen=gen,
+                               leg=leg:
+                               on_button_click(name,type1,type2,
+                                               hp,atk,defense,
+                                               spAtk,spDef,spd,gen,leg))
         button.pack(pady=5)
 
     canvas.update_idletasks()
     frameButtons.grid_propagate(True)
-    canvas.pack( fill = "both", expand = True)
+    canvas.pack( fill="both",expand=True)
     canvas.configure(scrollregion=canvas.bbox("all"))
 
 
-def on_button_click(name, type1, type2, hp):
+def on_button_click(name,type1,type2,hp,atk,defense,spAtk,spDef,spd,gen,leg):
     global currentScene
-    for widget in frameButtons.winfo_children():
-        widget.destroy()
-    currentScene = "PokeDetails"
-    framePokeDetails.pack(side = "top", anchor = "nw", padx = 10, pady = 10)
-    pokeNameLabel.pack(padx = 5, pady = 5)
-    pokeTypeLabel.pack(padx=5, pady=5)
-    pokeType2Label.pack(padx=5, pady=5)
-    pokeHpLabel.pack(padx=5, pady=5)
-    pokeNameLabel.configure(text = "Name: " + name)
-    pokeTypeLabel.configure(text = "Type: " + type1)
+    remove_pokemon_()
+    currentScene="PokeDetails"
+    framePokeDetails.pack(side="top",anchor="ne",padx=10,pady=10)
+    pokeNameLabel.pack(padx=5,pady=5)
+    pokeTypeLabel.pack(padx=5,pady=5)
+    pokeType2Label.pack(padx=5,pady=5)
+    pokeHpLabel.pack(padx=5,pady=5)
+    pokeAtkLabel.pack(padx=5,pady=5)
+    pokeDefLabel.pack(padx=5,pady=5)
+    pokeSpAtkLabel.pack(padx=5,pady=5)
+    pokeSpDefLabel.pack(padx=5,pady=5)
+    pokeSpeedLabel.pack(padx=5,pady=5)
+    pokeGenLabel.pack(padx=5,pady=5)
+    pokeLegendLabel.pack(padx=5,pady=5)
+    pokeNameLabel.configure(text="Name: "+name)
+    pokeTypeLabel.configure(text="Type: "+type1)
     if type2:
-        pokeType2Label.configure(text = "Type 2: None")
+        pokeType2Label.configure(text="Type 2: None")
     else:
-        pokeType2Label.configure(text = "Type 2: " + type2)
-    pokeHpLabel.configure(text = "HP: " + str(hp))
-
+        pokeType2Label.configure(text="Type 2: "+type2)
+    pokeHpLabel.configure(text="HP: "+str(hp))
+    pokeAtkLabel.configure(text="Attack: "+str(atk))
+    pokeDefLabel.configure(text="Defense: "+str(defense))
+    pokeSpAtkLabel.configure(text="SP. Attack: "+str(spAtk))
+    pokeSpDefLabel.configure(text="SP. Defense: "+str(spDef))
+    pokeSpeedLabel.configure(text="Speed: "+str(spd))
+    pokeGenLabel.configure(text="Generation: "+str(gen))
+    pokeLegendLabel.configure(text="Legendary?  "+str(leg))
     canvas.configure(scrollregion=canvas.bbox("all"))
-
+    #HERE ALSO MAKE THE GRAPH BUTTON THAT MAKES A POLYGON GRAPH WITH STATS OPEN UP!!!
+    #WAKE UP LOOK
+    # WAKE UP LOOK
+    # WAKE UP LOOK
+    # WAKE UP LOOK
+    # WAKE UP LOOK
+    # WAKE UP LOOK
+    # WAKE UP LOOK
+    # WAKE UP LOOK
+    # WAKE UP LOOK
+    # WAKE UP LOOK
+    # WAKE UP LOOK
+    # WAKE UP LOOK
+    # WAKE UP LOOK
+    # WAKE UP LOOK
 
 def on_mouse_wheel(event):
     canvas.yview_scroll(int(-1*(event.delta/120)), "units")
@@ -109,7 +147,7 @@ def combobox_callback(choice):
 def back_button_():
     global currentScene
     if currentScene == "Main":
-        frameButtons.configure(height=900, width=1200)
+        frameButtons.configure(height=600, width=600)
         print()
     if currentScene == "PokeDetails":
         typeDistButton.pack(pady = 10, padx = 10, side = "top", anchor = "w")
@@ -121,7 +159,7 @@ def back_button_():
         currentScene = "Main"
     if currentScene == "PokeList":
         typeDistButton.pack(pady = 10, padx = 10, side = "top", anchor = "w")
-        frameButtons.configure(height=900, width=1200)
+        frameButtons.configure(height=600, width=600)
         for widget in frameButtons.winfo_children():
             widget.destroy()
         currentScene = "Main"
@@ -142,8 +180,6 @@ def hide_me_(event):
 
 
 def graph_():
-
-
     typesCombined = pd.concat([data["Type 1"], data["Type 2"]])
     valueCount = typesCombined.value_counts()
     print(valueCount)
@@ -187,7 +223,7 @@ def graph_():
 
 root = ctk.CTk()
 root.title("Pokedex")
-root.geometry("1200x900")
+root.geometry("600x600")
 root.configure(bg="#b34448", fg_color="#b34448")
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
@@ -206,12 +242,12 @@ label = ctk.CTkLabel(searchFrame, text = "Pokedex")
 label.pack(pady = 10)
 #Create the entry field
 
-entry = ctk.CTkEntry(searchFrame, width = 900,)
+entry = ctk.CTkEntry(searchFrame, width = 450,)
 entry.pack(side = "left", padx = 10, pady = 5)
 
 searchButton = ctk.CTkButton(searchFrame, text = "Search",
                              command = filter_pokemon_,
-                          width=300, fg_color="#963035")
+                          width=150, fg_color="#963035")
 searchButton.pack(side = "left", padx = 10, pady = 5)
 
 #comboboxVar = ttk.StringVar(value="Type")
@@ -235,14 +271,14 @@ backButton.pack(side = "bottom", pady=10)
 
 
 
-frameButtons = ctk.CTkFrame(canvas, width = 1205, height = 900, fg_color= "#b34448", border_width=0,)
+frameButtons = ctk.CTkFrame(canvas, width = 600, height = 600, fg_color= "#b34448", border_width=0,)
 
 
 scrollbar = ctk.CTkScrollbar(canvas, command = canvas.yview)
 scrollbar.pack(side = "right", fill = "y")
 canvas.configure(yscrollcommand = scrollbar.set)
 
-canvas.create_window((0, 0), window=frameButtons, anchor="nw", width = 1200)
+canvas.create_window((0, 0), window=frameButtons, anchor="nw", width = 600)
 
 frameButtons.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
 root.bind_all("<MouseWheel>", on_mouse_wheel)
@@ -250,18 +286,23 @@ root.bind_all("<MouseWheel>", on_mouse_wheel)
 
 
 
-framePokeDetails = ctk.CTkFrame(canvas, width = 1200, height = 900)
+framePokeDetails = ctk.CTkFrame(canvas, width = 600, height = 600)
 framePokeDetails.pack(side = "top", anchor = "nw", padx = 10, pady = 10)
 framePokeDetails.pack_forget()
 #Create the search button
 pokeNameLabel = ctk.CTkLabel(framePokeDetails, text = "No Name Set")
-pokeNameLabel.pack_forget()
 pokeTypeLabel = ctk.CTkLabel(framePokeDetails, text = "No Type Set")
-pokeNameLabel.pack_forget()
 pokeType2Label = ctk.CTkLabel(framePokeDetails, text = "No Type 2 Set")
 pokeHpLabel = ctk.CTkLabel(framePokeDetails, text = "No HP Value Set")
+pokeAtkLabel = ctk.CTkLabel(framePokeDetails, text = "No Attack Set")
+pokeDefLabel = ctk.CTkLabel(framePokeDetails, text = "No Defense Set")
+pokeSpAtkLabel = ctk.CTkLabel(framePokeDetails, text = "No Special Attack Set")
+pokeSpDefLabel = ctk.CTkLabel(framePokeDetails, text = "No Special Defense Set")
+pokeSpeedLabel = ctk.CTkLabel(framePokeDetails, text = "No Speed Set")
+pokeGenLabel = ctk.CTkLabel(framePokeDetails, text = "No Generation Set")
+pokeLegendLabel = ctk.CTkLabel(framePokeDetails, text = "No Legendary Bool Set")
 
-frameButtons.configure(height=900, width=1200)
+frameButtons.configure(height=600, width=600)
 
 resultLabel = ctk.CTkLabel(root,text="",justify="left",font=(font1, 10))
 resultLabel.place(x=20,y=100)
